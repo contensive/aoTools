9574b7a6cd2bf23729693e337b9d0e8e76e226db
jkidwell <jay@contensive.com>
2023-09-08 00:07:06 -0400

    move addon mock to design blocks

--- server/tools/Addons/AddonMockClass.cs
+++ server/tools/Addons/AddonMockClass.cs
@@ -1,64 +1,64 @@
 ﻿
-using Contensive.Addons.Tools.Controllers;
-using Contensive.Addons.Tools.Models.Db;
-using Contensive.BaseClasses;
-using Models.View;
-using System;
+//using Contensive.Addons.Tools.Controllers;
+//using Contensive.Addons.Tools.Models.Db;
+//using Contensive.BaseClasses;
+//using Models.View;
+//using System;
 
-namespace Contensive.Addons.Tools {
-    // 
-    // ====================================================================================================
-    //
-    public class AddonMockClass : AddonBaseClass {
-        // 
-        // ====================================================================================================
-        // 
-        public override object Execute(CPBaseClass CP) {
-            // 
-            // -- read instanceId, guid created uniquely for this instance of the addon on a page
-            var result = string.Empty;
-            AddonMockDataModel settings = null;
-            try {
-                //
-                // -- this is a dev tool -- catch excerptions and display
-                // -- edit tool is outside to let them edit content on exception
-                var settingsGuid = DesignBlockController.getSettingsGuid(CP, "Addon Mock", ref result);
-                if (string.IsNullOrEmpty(settingsGuid)) {
-                    return result;
-                }
-                // 
-                // -- locate or create a data record for this guid
-                settings = AddonMockDataModel.createOrAddSettings(CP, settingsGuid);
-                if (settings == null) {
-                    throw new ApplicationException("Could not create the design block settings record.");
-                }
-                // 
-                // -- translate the Db model to a view model and mustache it into the layout
-                var viewModel = AddonMockViewModel.create(CP, settings);
-                if (viewModel == null) {
-                    throw new ApplicationException("Could not create design block view model.");
-                }
-                object dataObject = Newtonsoft.Json.JsonConvert.DeserializeObject(viewModel.jsonData);
-                result = Nustache.Core.Render.StringToString(viewModel.layout, dataObject);
-            } catch (ArgumentException ex) {
-                //
-                // -- argument error
-                result = "<div class=\"ccError\">" + ex.Message + "</div>";
-            } catch (ApplicationException ex) {
-                //
-                // -- app error
-                result = "<div class=\"ccError\">" + ex.Message + "</div>";
-            } catch (Exception ex) {
-                //
-                // -- tool, just return the exception
-                result = "<div class=\"ccError\">" + ex.Message + "</div>";
-            }
-            // 
-            // -- if editing enabled, add the link and wrapperwrapper
-            if (settings == null) { 
-                return result; 
-            }
-            return CP.Content.GetEditWrapper(result, AddonMockDataModel.tableMetadata.contentName, settings.id);
-        }
-    }
-}
+//namespace Contensive.Addons.Tools {
+//    // 
+//    // ====================================================================================================
+//    //
+//    public class AddonMockClass : AddonBaseClass {
+//        // 
+//        // ====================================================================================================
+//        // 
+//        public override object Execute(CPBaseClass CP) {
+//            // 
+//            // -- read instanceId, guid created uniquely for this instance of the addon on a page
+//            var result = string.Empty;
+//            AddonMockDataModel settings = null;
+//            try {
+//                //
+//                // -- this is a dev tool -- catch excerptions and display
+//                // -- edit tool is outside to let them edit content on exception
+//                var settingsGuid = DesignBlockController.getSettingsGuid(CP, "Addon Mock", ref result);
+//                if (string.IsNullOrEmpty(settingsGuid)) {
+//                    return result;
+//                }
+//                // 
+//                // -- locate or create a data record for this guid
+//                settings = AddonMockDataModel.createOrAddSettings(CP, settingsGuid);
+//                if (settings == null) {
+//                    throw new ApplicationException("Could not create the design block settings record.");
+//                }
+//                // 
+//                // -- translate the Db model to a view model and mustache it into the layout
+//                var viewModel = AddonMockViewModel.create(CP, settings);
+//                if (viewModel == null) {
+//                    throw new ApplicationException("Could not create design block view model.");
+//                }
+//                object dataObject = Newtonsoft.Json.JsonConvert.DeserializeObject(viewModel.jsonData);
+//                result = Nustache.Core.Render.StringToString(viewModel.layout, dataObject);
+//            } catch (ArgumentException ex) {
+//                //
+//                // -- argument error
+//                result = "<div class=\"ccError\">" + ex.Message + "</div>";
+//            } catch (ApplicationException ex) {
+//                //
+//                // -- app error
+//                result = "<div class=\"ccError\">" + ex.Message + "</div>";
+//            } catch (Exception ex) {
+//                //
+//                // -- tool, just return the exception
+//                result = "<div class=\"ccError\">" + ex.Message + "</div>";
+//            }
+//            // 
+//            // -- if editing enabled, add the link and wrapperwrapper
+//            if (settings == null) { 
+//                return result; 
+//            }
+//            return CP.Content.GetEditWrapper(result, AddonMockDataModel.tableMetadata.contentName, settings.id);
+//        }
+//    }
+//}
--- server/tools/Models/Db/AddonMockDataModel.cs
+++ server/tools/Models/Db/AddonMockDataModel.cs
@@ -1,75 +1,75 @@
 ﻿
-using Contensive.BaseClasses;
-using Contensive.Models.Db;
-using System;
+//using Contensive.BaseClasses;
+//using Contensive.Models.Db;
+//using System;
 
-namespace Contensive.Addons.Tools {
-    namespace Models.Db {
-        public class AddonMockDataModel : Contensive.DesignBlockBase.Models.Db.SettingsBaseModel {
-            /// <summary>
-            /// table definition
-            /// </summary>
-            public static DbBaseTableMetadataModel tableMetadata { get; } = new DbBaseTableMetadataModel("Add-on Mock Data", "ccAddonMockData", "default", false);
-            //
-            //====================================================================================================
-            //
-            /// <summary>
-            /// json string pasted in the settings record to simulate server code
-            /// </summary>
-            public string jsondata { get; set; }
-            /// <summary>
-            /// the layout to be merged with the dataq
-            /// </summary>
-            public int layoutid { get; set; }
-            // 
-            // ====================================================================================================
-            public static AddonMockDataModel createOrAddSettings(CPBaseClass cp, string settingsGuid) {
-                AddonMockDataModel result = create<AddonMockDataModel>(cp, settingsGuid);
-                if ((result == null)) {
-                    // 
-                    // -- create default content
-                    result = addDefault<AddonMockDataModel>(cp);
-                    result.name = tableMetadata.contentName + " " + result.id;
-                    result.ccguid = settingsGuid;
-                    result.themeStyleId = 0;
-                    result.padTop = false;
-                    result.padBottom = false;
-                    result.padRight = false;
-                    result.padLeft = false;
-                    // 
-                    // -- create custom content
-                    result.jsondata = "{\"headline\":\"Hello World\",\"items\":[{\"name\":\"Rock\",\"price\":\"$1.24\"},{\"name\":\"Nice Rock\",\"price\":\"$2.99\"}]}";
-                    //
-                    DateTime rightNow = DateTime.Now;
-                    var layout = DbBaseModel.create<LayoutModel>(cp, Constants.guidAddonMockLayout);
-                    string defaultLayout = "<h2>{{headline}}</h2><ul>{{#items}}<hr><li><div>name: {{name}}</div><div>price: {{price}}</div></li>{{/items}}</ul><hr>";
-                    if (layout == null) {
-                        //
-                        // -- no layout found for the default guid, create now
-                        layout = DbBaseModel.addDefault<LayoutModel>(cp);
-                        layout.name = "Addon Mock Default Layout, " + rightNow.Year + rightNow.Month.ToString().PadLeft(2, '0') + rightNow.Day.ToString().PadLeft(2, '0');
-                        layout.ccguid = Constants.guidAddonMockLayout;
-                        layout.layout.content = defaultLayout;
-                        layout.save(cp);
-                    }
-                    if(layout.layout.content != defaultLayout) {
-                        //
-                        // -- layout does not match default content, they edited it so just create a new record
-                        layout = DbBaseModel.addDefault<LayoutModel>(cp);
-                        layout.name = "Addon Mock Default Layout, " + rightNow.Year + rightNow.Month.ToString().PadLeft(2, '0') + rightNow.Day.ToString().PadLeft(2, '0');
-                        layout.ccguid = cp.Utils.CreateGuid();
-                        layout.layout.content = defaultLayout;
-                        layout.save(cp);
-                    }
-                    result.layoutid = layout.id;
-                    // 
-                    result.save(cp);
-                    // 
-                    // -- track the last modified date
-                    cp.Content.LatestContentModifiedDate.Track(result.modifiedDate);
-                }
-                return result;
-            }
-        }
-    }
-}
+//namespace Contensive.Addons.Tools {
+//    namespace Models.Db {
+//        public class AddonMockDataModel : Contensive.DesignBlockBase.Models.Db.SettingsBaseModel {
+//            /// <summary>
+//            /// table definition
+//            /// </summary>
+//            public static DbBaseTableMetadataModel tableMetadata { get; } = new DbBaseTableMetadataModel("Add-on Mock Data", "ccAddonMockData", "default", false);
+//            //
+//            //====================================================================================================
+//            //
+//            /// <summary>
+//            /// json string pasted in the settings record to simulate server code
+//            /// </summary>
+//            public string jsondata { get; set; }
+//            /// <summary>
+//            /// the layout to be merged with the dataq
+//            /// </summary>
+//            public int layoutid { get; set; }
+//            // 
+//            // ====================================================================================================
+//            public static AddonMockDataModel createOrAddSettings(CPBaseClass cp, string settingsGuid) {
+//                AddonMockDataModel result = create<AddonMockDataModel>(cp, settingsGuid);
+//                if ((result == null)) {
+//                    // 
+//                    // -- create default content
+//                    result = addDefault<AddonMockDataModel>(cp);
+//                    result.name = tableMetadata.contentName + " " + result.id;
+//                    result.ccguid = settingsGuid;
+//                    result.themeStyleId = 0;
+//                    result.padTop = false;
+//                    result.padBottom = false;
+//                    result.padRight = false;
+//                    result.padLeft = false;
+//                    // 
+//                    // -- create custom content
+//                    result.jsondata = "{\"headline\":\"Hello World\",\"items\":[{\"name\":\"Rock\",\"price\":\"$1.24\"},{\"name\":\"Nice Rock\",\"price\":\"$2.99\"}]}";
+//                    //
+//                    DateTime rightNow = DateTime.Now;
+//                    var layout = DbBaseModel.create<LayoutModel>(cp, Constants.guidAddonMockLayout);
+//                    string defaultLayout = "<h2>{{headline}}</h2><ul>{{#items}}<hr><li><div>name: {{name}}</div><div>price: {{price}}</div></li>{{/items}}</ul><hr>";
+//                    if (layout == null) {
+//                        //
+//                        // -- no layout found for the default guid, create now
+//                        layout = DbBaseModel.addDefault<LayoutModel>(cp);
+//                        layout.name = "Addon Mock Default Layout, " + rightNow.Year + rightNow.Month.ToString().PadLeft(2, '0') + rightNow.Day.ToString().PadLeft(2, '0');
+//                        layout.ccguid = Constants.guidAddonMockLayout;
+//                        layout.layout.content = defaultLayout;
+//                        layout.save(cp);
+//                    }
+//                    if(layout.layout.content != defaultLayout) {
+//                        //
+//                        // -- layout does not match default content, they edited it so just create a new record
+//                        layout = DbBaseModel.addDefault<LayoutModel>(cp);
+//                        layout.name = "Addon Mock Default Layout, " + rightNow.Year + rightNow.Month.ToString().PadLeft(2, '0') + rightNow.Day.ToString().PadLeft(2, '0');
+//                        layout.ccguid = cp.Utils.CreateGuid();
+//                        layout.layout.content = defaultLayout;
+//                        layout.save(cp);
+//                    }
+//                    result.layoutid = layout.id;
+//                    // 
+//                    result.save(cp);
+//                    // 
+//                    // -- track the last modified date
+//                    cp.Content.LatestContentModifiedDate.Track(result.modifiedDate);
+//                }
+//                return result;
+//            }
+//        }
+//    }
+//}
